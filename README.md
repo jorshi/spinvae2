1cda8c2c52ea931ad4424f12b9ecff69e801be26
jorshi <jordieshier@gmail.com>
2024-03-01 09:31:47 +0000

    Example config confidential

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Work in progress.
 
 Audio examples are available on the [companion website](https://gwendal-lv.github.io/spinvae2).
+
+## Installation Notes
+
+- Had to move data/regenerate.py to root directory
+- Add utils/config_confidential.py (Made an example config in utils)
+
---
+++ utils/config_confidential_example.py
@@ -0,0 +1,6 @@
+# Example of a confidential config file - fill in and rename to config_confidential.py
+comet_api_key = "your-comet-api-key-here"
+comet_project_name = "spinvae2"
+comet_workspace = "your-comet-workspace-here"
+data_root_path = "synth-dataset"
+logs_root_dir = "train_logs"
